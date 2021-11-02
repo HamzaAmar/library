b73e7fd7dd7ab13709b35752a2450f412eb3198b
Hamza Miloud Amar <hamzamiloudamar@gmail.com>
2021-11-02 04:26:54 +0100

    feat: add new book page

---
+++ src/pages/books/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from '@components/core';
+import React from 'react';
+
+const index = () => {
+  return (
+    <Layout>
+      <h1>Hello</h1>
+    </Layout>
+  );
+};
+
+export default index;
